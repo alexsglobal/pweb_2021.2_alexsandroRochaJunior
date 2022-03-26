65a1c120c6907ca01e9544ccbb4004e5df4d2bf2
alexsglobal <79956420+alexsglobal@users.noreply.github.com>
2022-03-26 14:18:52 -0300

    AdicionandoMVC

---
+++ u1_exercicio3/cadpessoas_alexs/src/main/java/br/com/alexs/cadpessoas/controllers/PessoaController.java
@@ -0,0 +1,11 @@
+package br.com.alexs.cadpessoas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PessoaController {
+
+
+}
---
+++ u1_exercicio3/cadpessoas_alexs/src/main/java/br/com/alexs/cadpessoas/repositories/PessoaRepository.java
@@ -0,0 +1,12 @@
+package br.com.alexs.cadpessoas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alexs.cadpessoas.model.Pessoa;
+
+@Repository
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
+}
