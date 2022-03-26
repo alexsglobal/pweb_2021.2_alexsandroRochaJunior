4a11b586f911ed1fcc8fb0eda04795fd364dee7d
alexsglobal <79956420+alexsglobal@users.noreply.github.com>
2022-03-26 10:46:36 -0300

    Descompactando na pasta u1_exercicio3
    Descompactando arquivo gerado pelo spring initializer

---
+++ u1_exercicio3/cadpessoas_alexs/src/main/java/br/com/alexs/cadpessoas/CadpessoasAlexsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexs.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasAlexsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasAlexsApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/cadpessoas_alexs/src/test/java/br/com/alexs/cadpessoas/CadpessoasAlexsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexs.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasAlexsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
